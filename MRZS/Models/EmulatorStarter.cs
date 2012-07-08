f017c11e556d31f7bf04a96bf0aba891b107a86c
Igor Bugaenko <igorbugaenko21@gmail.com>
2012-07-08 15:29:17 +0300

    change mrzs buttons text (arrows button), add button "Run emulator"

--- MRZS/Models/EmulatorStarter.cs
+++ MRZS/Models/EmulatorStarter.cs
@@ -14,14 +14,16 @@ namespace MRZS.Models
 {
     public class EmulatorStarter
     {
-        private string _appName = "KievPribor.exe";
-        private string _dirPath = @"D:\Projects\MRZS\Sources\QuizApplication\Emulator";
+        //private string _appName = "KievPribor.exe";
+        //private string _dirPath = @"D:\Projects\MRZS\Sources\QuizApplication\Emulator";
         public bool Start()
         {
-            dynamic shell = AutomationFactory.CreateObject("Shell.Application");
-            shell.ShellExecute(_appName, "", _dirPath);
-            //TODO: add error handling
+            //dynamic shell = AutomationFactory.CreateObject("Shell.Application");
+            //shell.ShellExecute(_appName, "", _dirPath);
+            ////TODO: add error handling
+
             return true;
         }
+
     }
 }
